4b4eacdc7e334f79b791daa781534cae9be50d45
Julia Rose <jmrose@pbs.org>
2018-12-28 14:16:24 -0500

    header updates after testing in bento environment

--- js/header.js
+++ js/header.js
@@ -1,10 +1,4 @@
-// FYI - JQuery included on page in header (first embed at top of page)
-$( document ).ready(function() {
-      
-  // --------------------------------------- //
-  // JavaScript for Mobile Menu //
-  // --------------------------------------- //
-  // Show and hide the Mobile Menu
+$( document ).ready(function() { 
   function toggleMobileMenu()  {
     $(".yp-header").removeClass("search-open");
     $(".yp-header").toggleClass("menu-open");
@@ -17,14 +11,4 @@ $( document ).ready(function() {
   // Attach click event listeners
   $('.yp-header__menu-button').click(toggleMobileMenu);
   $('.yp-header__search').click(toggleSearchDropdown);
-  
-  // Set correct page as selected
-  // if ($('.yp-about').length === 1) {
-  //   $("#nav--about").addClass("selected");
-  // } else if ($('.yp-events').length === 1 || $('.yp-event').length === 1) {
-  //   $("#nav--attend").addClass("selected");
-  // } else if ($('.blog-river__2-Wjj').length === 1 || $('.blog-entry').length === 1) {
-  //   $("#nav--learn").addClass("selected");
-  // }
-  
 });
